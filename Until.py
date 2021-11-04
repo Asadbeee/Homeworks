de36a7c23e9a996a1c17d196e0e9b59299796d76
Asadbeee <92811432+Asadbeee@users.noreply.github.com>
2021-11-04 21:37:23 +0500

    Add files via upload

---
+++ Until.py
@@ -0,0 +1,13 @@
+print('Привет,это бот для знакомства')
+sex = input('Введите ваш пол')
+if sex == ('Ж'):
+    age = int(input('Введите ваш возраст'))
+    if age >= 18:
+        print('Супер')
+        x = int(input())
+        y = int(input())
+        print(x,y)
+    else:
+        print('Идите лесом, за это меня посадят')
+else:
+    print('Я по девушкам')
